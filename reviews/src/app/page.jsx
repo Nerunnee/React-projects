989756789127ae31a62eff49b645431c91cb9c07
norovsuren15 <e.norvoo@gmail.com>
2026-01-07 12:37:27 +0800

    v1

---
+++ reviews/src/app/page.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div>
+      <PeopleReviews />
+    </div>
+  );
+}
+
+const peopleInfo = [
+  {
+    image: "/profile1.jpg",
+    name: "Bill Anderson",
+    job: "The boss",
+  },
+];
+
+const PeopleReviews = () => {};
