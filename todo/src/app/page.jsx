cb2c96ca8757fcb20478f46b90e6d75e9240aa24
norovsuren15 <e.norvoo@gmail.com>
2026-01-21 20:41:37 +0800

    v3

--- todo/src/app/page.jsx
+++ todo/src/app/page.jsx
@@ -4,7 +4,7 @@ const { useState } = require("react");
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-gray-300 min-h-screen flex flex-col items-center pt-20">
       <TodoList />
     </div>
   );
@@ -76,69 +76,81 @@ const TodoList = () => {
   };
 
   return (
-    <div className="w-94.25 py-6 px-4">
-      <h1>To-Do List</h1>
+    <div className="w-94.25 py-6 px-4 bg-white rounded-md flex flex-col items-center gap-8">
+      <div className="flex flex-col gap-5">
+        <h1 className="text-2xl text-center ">To-Do List</h1>
 
-      <input
-        type="text"
-        placeholder="text"
-        className="border"
-        value={value}
-        onChange={() => HandleChange(event)}
-      />
+        <div className="flex gap-1.5">
+          <input
+            type="text"
+            placeholder="Add a new task..."
+            className="border rounded-md py-2 px-4 w-70"
+            value={value}
+            onChange={() => HandleChange(event)}
+          />
 
-      <button className="border" onClick={AddTask}>
-        Add
-      </button>
-
-      <div className="flex gap-2">
-        {buttons.map((filter, index) => (
           <button
-            key={index}
-            className="border"
-            onClick={() => onChangeFilter(filter)}
+            className="py-2 px-4 rounded-md bg-blue-500 text-white"
+            onClick={AddTask}
           >
-            {filter}
+            Add
           </button>
-        ))}
-      </div>
+        </div>
 
-      <div>
-        {filteredTasks.map((task, id) => {
-          return (
-            <div key={id} className="flex gap-4">
-              <div className="flex">
-                <input
-                  type="checkbox"
-                  name="checkbox"
-                  checked={task.checked}
-                  onChange={() => toggleChecked(task.id)}
-                />
-                <p className={`${task.checked && "line-through"}`}>
-                  {task.text}
-                </p>
-              </div>
+        <div className="flex gap-2">
+          {buttons.map((filter, index) => (
+            <button
+              key={index}
+              className="rounded-md bg-gray-200 py-1 px-3 hover:bg-blue-500"
+              onClick={() => onChangeFilter(filter)}
+            >
+              {filter}
+            </button>
+          ))}
+        </div>
 
-              <button
-                className="text-red-600"
-                onClick={() => deleteTask(task.id)}
+        <div>
+          {filteredTasks.map((task, id) => {
+            return (
+              <div
+                key={id}
+                className="flex justify-between items-center w-86.25 h-15.5 bg-gray-200 rounded-md p-4 mb-5"
               >
-                delete
-              </button>
-            </div>
-          );
-        })}
+                <div className="flex gap-2.5 items-center">
+                  <input
+                    type="checkbox"
+                    name="checkbox"
+                    checked={task.checked}
+                    onChange={() => toggleChecked(task.id)}
+                    className={`w-5 h-5 ${task.checked && "bg-blue-500"}`}
+                  />
+                  <p className={`${task.checked && "line-through"}`}>
+                    {task.text}
+                  </p>
+                </div>
+
+                <button
+                  className="text-red-600 bg-red-200 py-1.5 px-3 rounded-md"
+                  onClick={() => deleteTask(task.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
+
+        <div>
+          <p>
+            {completedCount} of {tasks.length} tasks completed
+          </p>
+          <button onClick={() => clearCompleted()} className="text-red-600">
+            Clear completed
+          </button>
+        </div>
       </div>
 
-      <div>
-        <p>
-          {" "}
-          {completedCount} of {tasks.length} tasks completed
-        </p>
-        <button onClick={() => clearCompleted()} className="text-red-600">
-          Clear completed
-        </button>
-      </div>
+      <p>Powered by Pinecone academy</p>
     </div>
   );
 };
